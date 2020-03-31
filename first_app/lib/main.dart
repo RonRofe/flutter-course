06c81777f8bd81c45f8de3ed98c29f0715fb13ba
Ron <ron@rapitec.co.il>
2020-03-31 12:34:04 +0300

    stateless widget

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex++;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     List<String> questions = [
       'What\'s your favorite color?',
-      'What\'s your favorite animal?'
+      'What\'s your favorite animal?',
     ];
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First App')),
-      body: Column(children: [
-        Text('The question!'),
-        RaisedButton(child: Text('Answer 1'), onPressed: null),
-        RaisedButton(child: Text('Answer 2'), onPressed: null),
-        RaisedButton(child: Text('Answer 3'), onPressed: null)
-      ])
-    )); 
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!')),
+            RaisedButton(child: Text('Answer 3'), onPressed: () {
+                // ...
+                print('Answer 3 chosen');
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
