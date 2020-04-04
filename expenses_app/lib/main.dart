b0900dc6adce3d2b69779642e5b00df3307df9dd
Ron <ron@rapitec.co.il>
2020-04-04 10:09:12 +0300

    main-axis-alignment & cross-axis-alignment

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -20,16 +20,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
        title: Text('Flutter App') 
       ),
-      body: Column(children: <Widget>[
-        Container(
-          width: double.infinity,
-          child: Card(
-            color: Colors.blue,
-            child: Text('CHART!'), 
-            elevation: 5,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'), 
+              elevation: 5,
+            ),
           ),
+        Card(
+          color: Colors.red,
+          child: Text('LIST OF TX'),
         ),
-        Card(child: Text('LIST OF TX'),),
       ])
     );
   }
