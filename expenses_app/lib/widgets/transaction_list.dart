bf3e44ce73c1ef8b376082e516c52a6f6c484819
Ron <ron@rapitec.co.il>
2020-04-11 20:59:05 +0300

    ListView + ListView.builder

--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -10,33 +10,37 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: this.transactions.map(
-        (Transaction transaction) => Card(child: Row(children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              '\$${transaction.amount}',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
-            ),
-            decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                transaction.title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18,)
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: this.transactions.length,
+        itemBuilder: (ctx, index) {
+          return Card(child: Row(children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              padding: EdgeInsets.all(10),
+              child: Text(
+                '\$${this.transactions[index].amount}',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
               ),
-              Text(
-                DateFormat.yMMMd().format(transaction.date),
-                style: TextStyle(color: Colors.grey)
-              )
-            ],
-            crossAxisAlignment: CrossAxisAlignment.start,
-          )
-        ],),)
-      ).toList(),
+              decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                  this.transactions[index].title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18,)
+                ),
+                Text(
+                  DateFormat.yMMMd().format(this.transactions[index].date),
+                  style: TextStyle(color: Colors.grey)
+                )
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )
+          ],),);
+        },
+      ),
     );
   }
 }
